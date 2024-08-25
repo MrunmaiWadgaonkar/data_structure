180b305a47ef9601446859627c18fdd477a2a1a8
MrunmaiWadgaonkar <mrunmaiwadgaonkar@gmail.com>
2024-08-25 19:25:28 +0530

    Add files via upload
    Signed-off-by: MrunmaiWadgaonkar <mrunmaiwadgaonkar@gmail.com>

---
+++ delete_beglink.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node 
+{
+    int data;
+    struct node *next;
+};
+struct node *s=NULL;
+void Delete_beg() 
+{
+    struct node *r;
+    if(s==NULL)
+    {
+    	printf("underflow\n");
+    	return(3);
+	}
+	else
+    {
+    	r=s;
+    	s=r->next;
+    	free(r);
+	}
+}
+int main() 
+{
+    char ch;
+    struct node *p=NULL; 
+    struct node *q=NULL;
+    do {
+        q=(struct node *)malloc(sizeof(struct node));
+        printf("enter data in node\n");
+        scanf("%d",&q->data);
+        q->next=NULL;
+        if(s==NULL) 
+		{
+            s=q;
+        } 
+		else 
+		{
+            p->next=q;
+        }
+        p=q;
+        printf("enter 'y' to continue\n");
+        scanf(" %c",&ch);
+    }while(ch=='y'||ch=='Y');
+    Delete_beg();
+    printf("the list is\n");
+    p=s;
+    while(p!=NULL) 
+	{
+        printf("%d\t",p->data);
+        p=p->next;
+    }
+    printf("\n");
+    while (s!=NULL) 
+	{
+        p=s;
+        s=s->next;
+        free(p);
+    }
+    return 0;
+}
